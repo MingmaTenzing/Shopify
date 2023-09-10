51648d1c62651253602e2ffa309e5cb45b3ac4b0
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-10 09:06:21 +0800

    collectioncs complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Nav from '../../components/Nav'
 import Header from '../../components/Header'
+import Collections from '../../components/Collections'
 
 export default function Home() {
   return (
     <main>
       <Nav /> 
       <Header />
+      <Collections />
     </main>
   )
 }
