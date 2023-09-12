78b9f950a9200b85be17162c710a486e6cedf930
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-12 12:39:33 +0800

    top laptops complete

--- components/TopLaptops.tsx
+++ components/TopLaptops.tsx
@@ -23,8 +23,8 @@ function TopLaptops({}: Props) {
   console.log(featuredLaptops);
 
   return (
-    <div className=" lg:flex  lg:flex-row-reverse relative">
-      <div className="  space-y-4 md:w-[90%] lg:w-auto m-auto">
+    <div className=" space-y-4 md:space-y-0 md:flex  lg:justify-between md:flex-row-reverse w-full items-center relative">
+      <div className="  space-y-4 md:w-[60%] lg:w-auto  m-auto">
         <div className=" relative lg:p-0 p-4">
           <h2 className="uppercase  font-semibold text-5xl text-gray-200">
             {" "}
@@ -32,7 +32,27 @@ function TopLaptops({}: Props) {
           </h2>
           <p className=" uppercase text-lg absolute top-10 lg:top-6">Woodstock Choice</p>
         </div>
-        <div className=" flex flex-col items-center md:flex-row md:items-start md:justify-center">
+        <div className=" ">
+          <span className=" lg:hidden flex  justify-center flex-wrap">
+          {featuredLaptops?.slice(0, 2).map((device) => (
+            <ProductContainer 
+              id={device.id}
+              description={device.description}
+              stock={device.stock}
+              brand={device.brand}
+              category={device.category}
+              images={device.images}
+              key={device.id}
+              rating={device.rating}
+              thumbnail={device.thumbnail}
+              title={device.title}
+              price={device.price}
+              discountPercentage={device.discountPercentage}
+            />
+          ))}
+
+          </span>
+          <span className=" hidden lg:flex">
           {featuredLaptops?.slice(0, 3).map((device) => (
             <ProductContainer 
               id={device.id}
@@ -49,6 +69,9 @@ function TopLaptops({}: Props) {
               discountPercentage={device.discountPercentage}
             />
           ))}
+
+          </span>
+          
         </div>
         <div className=" flex justify-center ">
           <button className=" cursor-pointer border-blue-500 border text-blue-500 px-3 py-2 rounded-lg">
@@ -56,9 +79,9 @@ function TopLaptops({}: Props) {
           </button>
         </div>
       </div>
-      <div className=" h-[100%]">
+      <div className="  md:w-[40%] lg:w-auto ">
 
-      <Image src={laptopBanne} alt=" laptops" className=" md:h-[320px] lg:w-[400px]  lg:h-[504px] object-cover object-center" />
+      <Image src={laptopBanne} alt=" laptops" className="   md:w-[90%] md:h-[500px]  lg:h-[504px] object-cover object-center" />
       </div>
     </div>
   );
