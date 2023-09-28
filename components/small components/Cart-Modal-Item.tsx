b7a32c26556afebf1e0b87059ac8e30073be569f
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-28 15:46:48 +0800

    global redux is working now

--- components/small components/Cart-Modal-Item.tsx
+++ components/small components/Cart-Modal-Item.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import NumberofItems from "../../utils/Number-of-Items";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import test from "../../assests/test1.webp"
-import { CartIem } from "../../types/cartItem-type";
 import { useAppDispatch } from "../../redux/hooks";
 import { deletefromCart } from "../../redux/slices/CartItems";
+import { CartItem } from "../../types/cartItem-type";
 
 type Props = {
-  cartItem: CartIem
+  cartItem: CartItem
 };
 function CartModalItem({cartItem}: Props) {
   const dispatch = useAppDispatch();
