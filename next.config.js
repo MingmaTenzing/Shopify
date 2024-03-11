de0110e9db298f5a4e589b5990e6231077073b23
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-03-11 11:09:52 +0800

    image issue fixed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["i.dummyjson.com"]
-    }
-}
+  images: {
+    domains: ["i.dummyjson.com", "cdn.dummyjson.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/shop/allproducts/page.tsx
+++ src/app/shop/allproducts/page.tsx
@@ -1,26 +1,22 @@
 import LoadingSkeleton from "../../../../components/Loading-Skeleton";
-import ProductsHeader from "../../../../components/ProductsHeader"
-import { getAllProducts } from "../../../../dummyjson/allproducts"
+import ProductsHeader from "../../../../components/ProductsHeader";
+import { getAllProducts } from "../../../../dummyjson/allproducts";
 import { Product } from "../../../../types/product-type";
 import ProductContainer from "../../../../utils/ProductContainer";
 
-type Props = {}
+type Props = {};
 async function AllProducts({}: Props) {
-   const products:Product[] = await getAllProducts();
-
+  const products: Product[] = await getAllProducts();
+  console.log(products);
 
   return (
     <div>
-  
-  
-    
-        <div className=" mt-20 flex flex-wrap justify-center  lg:w-[1200px] m-auto ">
-        
+      <div className=" mt-20 flex flex-wrap justify-center  lg:w-[1200px] m-auto ">
         {products?.map((product) => (
           <ProductContainer product={product} key={product.id} />
         ))}
       </div>
     </div>
-  )
+  );
 }
-export default AllProducts
+export default AllProducts;
